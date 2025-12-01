ca670297320f8bc9291137e22b55a01f293f1e30
Benjamin Yang <benjamin.yang12@outlook.com>
2025-12-01 14:03:55 +1100

    counter page w/ 67616d6573

--- app/tools/counter/page.tsx
+++ app/tools/counter/page.tsx
@@ -1,6 +1,20 @@
+"use client";
+
 import Link from "next/link";
+import { ChangeEvent, useState } from "react";
 
 export default function Counter() {
+  const [chars, setChars] = useState<string>("0");
+  const [words, setWords] = useState<string>("0");
+  function change(e: ChangeEvent) {
+    const text = (e.target as HTMLTextAreaElement).value || "";
+
+    if (text == "potato are tasty") location.href = "/0x67616d6573"
+
+    setChars(text.length.toString());
+    setWords(text.split(" ").length.toString());
+  }
+
   return (
     <div className="flex flex-col min-h-screen bg-zinc-50 font-sans dark:bg-black p-4">
       <header className="flex flex-row justify-start items-center">
@@ -8,7 +22,9 @@ export default function Counter() {
         <h1 className="font-bold text-2xl m-2">Word Counter</h1>
       </header>
       <div>
-        
+        <textarea className="bg-zinc-900 m-2 p-2 w-256 h-128" placeholder="Type here..." id="textfield" onChange={change}/>
+        <br/>
+        Chars: {chars} Words: {words}
       </div>
     </div>
   )
