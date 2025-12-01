fa1c01a224dad828fd303e3e4f7a2628b50879d2
Benjamin Yang <benjamin.yang12@outlook.com>
2025-12-01 11:43:17 +1100

    lnk in tools card

--- app/components/ToolCard.tsx
+++ app/components/ToolCard.tsx
@@ -1,13 +1,18 @@
+import Link from "next/link";
+
 interface ToolCardProps {
   title: string;
   desc: string;
+  url: string;
 }
 
-export function ToolCard({title, desc}: ToolCardProps) {
+export function ToolCard({title, desc, url}: ToolCardProps) {
   return (
-    <div className="p-2 m-1 bg-zinc-900 hover:bg-zinc-800 hover:cursor-pointer rounded-md">
-      <h2 className="text-xl">{title}</h2>
-      <p>{desc}</p>
-    </div>
+    <Link href={url}>
+      <div className="p-2 m-1 bg-zinc-900 hover:bg-zinc-800 hover:cursor-pointer rounded-md">
+        <h2 className="text-xl">{title}</h2>
+        <p>{desc}</p>
+      </div>
+    </Link>
   )
 }
