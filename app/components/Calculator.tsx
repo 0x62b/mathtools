bdd9cfce5a8e0ac62de4cd1a31d32bdba85d5bfc
Benjamin Yang <benjamin.yang12@outlook.com>
2025-12-02 11:14:32 +1100

    seperate calculator component

---
+++ app/components/Calculator.tsx
@@ -0,0 +1,60 @@
+import { FormEvent } from "react";
+import * as math from "mathjs";
+
+export default function Calculator() {
+  function submit(e: FormEvent) {
+    e.preventDefault();
+    const input = ((e.target as HTMLFormElement).children[0] as HTMLInputElement);
+    input.value = math.evaluate(input.value);
+  }
+
+  return (
+    <div>
+      <form className="w-full" onSubmit={submit}>
+        <input type="text" className="bg-zinc-900 p-2 m-1 rounded-md w-full" placeholder="Calculate..." id="calculation"/>
+        <input type="submit" hidden id="calculate"/>
+      </form>
+      <div className="grid grid-cols-5">
+        {["+", "-", "*", "/",      "sin(",
+          "9", "8", "7", "^",      "cos(",
+          "6", "5", "4", "%",      "tan(",
+          "3", "2", "1", "sqrt(", "(",
+          "!", "0", ".", "cbrt(", ")"].map(a => (
+          <button
+            key={a}
+            className="bg-zinc-900 hover:bg-zinc-800 p-2 m-1 rounded-sm"
+            onClick={() => (document.getElementById("calculation") as HTMLInputElement).value += a}
+          >
+            {a}
+          </button>
+        ))}
+      </div>
+      <div className="grid grid-cols-5">
+        <button
+          className="bg-zinc-900 hover:bg-zinc-800 p-2 m-1 rounded-sm"
+          onClick={() => (document.getElementById("calculation") as HTMLInputElement).value = ""}
+        >
+          C
+        </button>
+        <button
+          className="bg-zinc-900 hover:bg-zinc-800 p-2 m-1 rounded-sm"
+          onClick={() => (document.getElementById("calculate") as HTMLButtonElement).click()}
+        >
+          =
+        </button>
+        <button
+          className="bg-zinc-900 hover:bg-zinc-800 p-2 m-1 rounded-sm"
+          onClick={() => 
+            (document.getElementById("calculation") as HTMLInputElement).value =
+              (document.getElementById("calculation") as HTMLInputElement).value.substring(
+                0,
+                (document.getElementById("calculation") as HTMLInputElement).value.length-1
+              )
+          }
+        >
+          &lt;-
+        </button>
+      </div>
+    </div>
+  )
+}
--- app/tools/calculator/page.tsx
+++ app/tools/calculator/page.tsx
@@ -1,16 +1,9 @@
 "use client";
 
 import Link from "next/link";
-import { FormEvent } from "react";
-import * as math from "mathjs";
+import Calculator from "../../components/Calculator";
 
-export default function Calculator() {
-  function submit(e: FormEvent) {
-    e.preventDefault();
-    const input = ((e.target as HTMLFormElement).children[0] as HTMLInputElement);
-    input.value = math.evaluate(input.value);
-  }
-
+export default function CalculatorPage() {
   return (
     <div className="flex flex-col min-h-screen bg-zinc-50 font-sans dark:bg-black p-4">
       <header className="flex flex-row justify-start items-center">
@@ -18,51 +11,7 @@ export default function Calculator() {
         <h1 className="font-bold text-2xl m-2">Calculator</h1>
       </header>
         <div className="w-[50%]">
-          <form className="w-full" onSubmit={submit}>
-            <input type="text" className="bg-zinc-900 p-2 m-1 rounded-md w-full" placeholder="Calculate..." id="calculation"/>
-            <input type="submit" hidden id="calculate"/>
-          </form>
-          <div className="grid grid-cols-5">
-            {["+", "-", "*", "/",      "sin(",
-              "9", "8", "7", "^",      "cos(",
-              "6", "5", "4", "%",      "tan(",
-              "3", "2", "1", "sqrt(", "(",
-              "!", "0", ".", "cbrt(", ")"].map(a => (
-              <button
-                key={a}
-                className="bg-zinc-900 hover:bg-zinc-800 p-2 m-1 rounded-sm"
-                onClick={() => (document.getElementById("calculation") as HTMLInputElement).value += a}
-              >
-                {a}
-              </button>
-            ))}
-          </div>
-          <div className="grid grid-cols-5">
-            <button
-              className="bg-zinc-900 hover:bg-zinc-800 p-2 m-1 rounded-sm"
-              onClick={() => (document.getElementById("calculation") as HTMLInputElement).value = ""}
-            >
-              C
-            </button>
-            <button
-              className="bg-zinc-900 hover:bg-zinc-800 p-2 m-1 rounded-sm"
-              onClick={() => (document.getElementById("calculate") as HTMLButtonElement).click()}
-            >
-              =
-            </button>
-            <button
-              className="bg-zinc-900 hover:bg-zinc-800 p-2 m-1 rounded-sm"
-              onClick={() => 
-                (document.getElementById("calculation") as HTMLInputElement).value =
-                  (document.getElementById("calculation") as HTMLInputElement).value.substring(
-                    0,
-                    (document.getElementById("calculation") as HTMLInputElement).value.length-1
-                  )
-              }
-            >
-              &lt;-
-            </button>
-          </div>
+          <Calculator/>
         </div>
     </div>
   )
