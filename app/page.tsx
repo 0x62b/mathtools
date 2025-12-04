99f3ee3ac6ae55b7f0ac13df5cce05a57ca4d22a
Benjamin Yang <benjamin.yang12@outlook.com>
2025-12-05 08:22:56 +1100

    (unfunctional) search bar/new pages

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,10 @@ import { ToolCard } from "./components/ToolCard";
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen items-center bg-zinc-50 font-sans dark:bg-black p-4">
-      <h1 className="font-bold text-3xl">MathTools</h1>
+      <header className="flex flex-row justify-between w-full">
+        <h1 className="font-bold text-3xl">MathTools</h1>
+        <input className="bg-zinc-900 p-2 m-1 rounded-md" placeholder="Search..."/>
+      </header>
       <div className="grid xl:grid-cols-6 lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-1">
         <ToolCard title="Calculator" desc="Just a calculator" url="/tools/calculator"/>
         <ToolCard title="Converter" desc="Convert between units" url="/tools/converter"/>
@@ -12,6 +15,9 @@ export default function Home() {
         <ToolCard title="RegEx" desc="Interpret regular expressions" url="/tools/regex"/>
         <ToolCard title="Text Converter" desc="Text to hex/binary etc" url="/tools/text"/>
         <ToolCard title="JSON Formatter" desc="Formats JSON 🤯" url="/tools/json"/>
+        <ToolCard title="Stopwatch/Timer" desc="Stopwatch and Timer" url="/tools/timer"/>
+        <ToolCard title="Roman Numeral Converter" desc="Converts Roman numerals" url="/tools/roman"/>
+        <ToolCard title="Number Base Converter" desc="Convert numbers between bases" url="/tools/base"/>
       </div>
     </div>
   );
