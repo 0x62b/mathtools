af9898b0c7a3db292086fc60d480ce3afd6e63b3
Benjamin Yang <benjamin.yang12@outlook.com>
2025-12-05 14:05:49 +1100

    roman to decimal

--- app/components/Roman.tsx
+++ app/components/Roman.tsx
@@ -1,9 +1,46 @@
 "use client";
 
+import { useEffect, useState } from "react";
+
 export default function Roman() {
+  const [input, setInput] = useState<string>("");
+  const [output, setOutput] = useState<string>("");
+
+  useEffect(() => {
+    const num = Number.parseInt(input);
+    if (Number.isNaN(num)) {
+      const map: { [key: string]: number } = {
+        I: 1, V: 5, X: 10, L: 50,
+        C: 100, D: 500, M: 1000
+      };
+      let sum = 0;
+      let prev = 0;
+      for (let i = input.length - 1; i >= 0; i--) {
+        const curr = map[input[i].toUpperCase()];
+        if (!curr) {
+          setOutput("Error");
+        }
+        if (curr < prev) {
+          sum -= curr;
+        } else {
+          sum += curr;
+        }
+        prev = curr;
+      }
+      setOutput(sum.toString());
+    } else {
+
+    }
+  }, [input])
+
   return (
-    <div>
-      
+    <div className="flex flex-col">
+      <input className="bg-zinc-800 p-2 m-1 rounded-md" placeholder="From..." onChange={(e) => setInput(e.target.value)}/>
+      <label>The type of numeral is auto-detected</label>
+      <div className="m-1 p-2 flex flex-col">
+        <b>Output</b>
+        <label>{output}</label>
+      </div>
     </div>
   )
 }
--- app/tools/roman/page.tsx
+++ app/tools/roman/page.tsx
@@ -3,12 +3,12 @@
 import Link from "next/link";
 import Roman from "@/app/components/Roman";
 
-export default function BasesPage() {
+export default function RomanPage() {
   return (
     <div className="flex flex-col min-h-screen bg-zinc-50 font-sans dark:bg-black p-4">
       <header className="flex flex-row justify-start items-center">
         <Link href="/" className="m-2 font-semibold">MathTools</Link>
-        <h1 className="font-bold text-2xl m-2">Base Converter</h1>
+        <h1 className="font-bold text-2xl m-2">Roman Numeral Converter</h1>
       </header>
       <div>
         <Roman/>
