01d809ed1a13587e8e412b527843411c64e11c74
Benjamin Yang <benjamin.yang12@outlook.com>
2025-12-11 12:06:12 +1100

    tic tac toe ui

--- app/0x67616d6573/tictactoe/page.tsx
+++ app/0x67616d6573/tictactoe/page.tsx
@@ -1,9 +1,38 @@
 "use client";
 
-export default function TicTacToe() {
+import { useState } from "react";
+
+function Tile({ x, setX }: { x: boolean; setX: React.Dispatch<React.SetStateAction<boolean>> }) {
+  const [text, setText] = useState(".");
+
+  const handleClick = () => {
+    if (text === ".") {
+      setText(x ? "X" : "O");
+      setX(!x);
+    }
+  };
+
   return (
-    <div>
-      
+    <div 
+      className="bg-zinc-800 hover:bg-zinc-700 rounded-md w-16 h-16 flex flex-row items-center justify-center cursor-pointer"
+      onClick={handleClick}
+    >
+      <span className="text-white font-semibold text-2xl">
+        {text}
+      </span>
     </div>
-  )
+  );
+}
+
+export default function TicTacToe() {
+  const [x, setX] = useState(true);
+
+  return (
+    <div className="grid grid-cols-3 gap-2 w-fit">
+      <Tile x={x} setX={setX} /><Tile x={x} setX={setX} /><Tile x={x} setX={setX} />
+      <Tile x={x} setX={setX} /><Tile x={x} setX={setX} /><Tile x={x} setX={setX} />
+      <Tile x={x} setX={setX} /><Tile x={x} setX={setX} /><Tile x={x} setX={setX} />
+      <button onClick={() => location.reload()}>Clear</button>
+    </div>
+  );
 }
