cb546832574e4fdaef5039ba93902d98c994e7d3
Benjamin Yang <benjamin.yang12@outlook.com>
2025-12-13 16:19:36 +1100

    unfunctional 2048 ui

--- app/0x67616d6573/2048/page.tsx
+++ app/0x67616d6573/2048/page.tsx
@@ -1,9 +1,34 @@
 "use client";
 
-export default function G2048() {
+import { useState } from "react";
+
+function Cell({ score }: { score: number }) {
   return (
-    <div>
-      
+    <div className="w-24 h-24 bg-zinc-800 m-1 flex items-center justify-center rounded-md">
+      {score ? score : ""}
     </div>
-  )
+  );
+}
+
+export default function G2048() {
+  const [values, setValues] = useState([
+    [0, 0, 0, 0],
+    [0, 0, 2, 0],
+    [0, 0, 0, 0],
+    [0, 0, 2, 0]
+  ]);
+  
+  return (
+    <div className="w-108 h-108 flex flex-col items-center justify-center rounded-md bg-zinc-900">
+      <div>
+        {values.map((row, rowIndex) => (
+          <div key={rowIndex} className="flex flex-row w-96 h-24 m-2">
+            {row.map((value, colIndex) => (
+              <Cell key={colIndex} score={value} />
+            ))}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
